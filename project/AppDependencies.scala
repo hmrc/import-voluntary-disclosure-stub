5fecb536a5182d9d269029b10d61b0dc44b7e13e
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-11-29 10:42:44 +0000

    updating dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.6.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.3"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.16.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.5"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.6.0" % Test,
-    "org.scalatest" %% "scalatest" % "3.2.3" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.16.0" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.10" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
-    "org.scalamock" %% "scalamock" % "4.4.0" % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
+    "org.scalamock" %% "scalamock" % "5.1.0" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2" % "test, it"
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.31.0" % "test, it"
   )
 }
