fcd8a426b95bd8cb0a248dfde7ab9fea9884cfcf
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-12-15 14:36:26 +0000

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.16.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.5"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.18.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.16.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.18.0" % Test,
     "org.scalatest" %% "scalatest" % "3.2.10" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
     "org.scalamock" %% "scalamock" % "5.1.0" % Test,
