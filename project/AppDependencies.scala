a43e2bc1c132e789040553283d56a7d41ebf57de
jpatton638 <jpatton638@users.noreply.github.com>
2022-02-09 14:04:54 +0000

    DDCYLS-426 | Library upgrades for stubs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.18.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.20.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.18.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.20.0" % Test,
     "org.scalatest" %% "scalatest" % "3.2.10" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
     "org.scalamock" %% "scalamock" % "5.1.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.31.0" % "test, it"
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.32.0" % "test, it"
   )
 }
